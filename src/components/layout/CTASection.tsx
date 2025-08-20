f4cf2b666d21fcde0a889afaf31d62d2ba45e9ee
aishaksl <aisha.koseoglu@gmail.com>
2025-08-20 18:01:24 +0700

    style: change texts color

--- src/components/layout/CTASection.tsx
+++ src/components/layout/CTASection.tsx
@@ -7,17 +7,19 @@ export const CTASection: React.FC = () => {
 
   return (
     <div className="h-full flex flex-col justify-center">
-      <div className="text-center bg-gradient-to-r from-purple-600/10 via-blue-600/10 to-green-600/10 rounded-3xl p-16 backdrop-blur-sm border border-white/30">
-        <h2 className="text-3xl md:text-4xl font-light text-slate-800 mb-6">
+      <div className="text-center rounded-3xl p-16 backdrop-blur-sm border border-white/30">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-extralight bg-clip-text text-transparent bg-gradient-to-br from-blue-300 to-purple-600 leading-tight tracking-tight mb-4 pb-1">
           Ready to Send Your First Message?
         </h2>
-        <p className="text-xl text-slate-600 mb-8 max-w-2xl mx-auto leading-relaxed">
+        <p className=" mb-8 text-lg md:text-xl text-slate-600 leading-relaxed font-light">
           Join thousands who trust us with their most meaningful messages
         </p>
         <Button
           onClick={() => navigate('/create-message')}
           size="lg"
-          className="bg-gradient-to-r from-purple-600 to-blue-600 hover:from-purple-700 hover:to-blue-700 text-white px-12 py-6 text-lg rounded-full shadow-lg hover:shadow-xl transition-all duration-300 group"
+          className="bg-gradient-to-r hover:from-cyan-200 hover:to-blue-300 
+               from-blue-200 to-purple-300 
+               text-white px-12 py-6 text-lg rounded-full shadow-lg hover:shadow-xl transition-all duration-300 group"
         >
           <MessageSquarePlus className="w-5 h-5 mr-2 group-hover:scale-110 transition-transform" />
           Create Your Message
